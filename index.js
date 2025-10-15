f5f494d9c8ddf5de1e5b749c8216796790f70576
Vardan Khanzadyan <vardan@Vardans-MacBook-Air.local>
2025-10-15 15:29:17 +0400

    Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {addUser, deleteUser, editUser, getAllUsers, getUser, searchUser} from "./lib/model.js";
+import {addUser, deleteUser, editUser, getAllUsers, getUser} from "./lib/model.js";
 
 const app = express()
 app.use(express.json())
@@ -19,19 +19,25 @@ app.get('/users/:id', async(req, res)=>{
     res.send({user:found})
 })
 
+
 app.post('/users', async(req, res)=>{
     await addUser({...req.body, id: Date.now()})
     res.status(201).send({ok:true})
 })
 
+
 app.patch('/users/:id', async(req, res)=>{
-    await editUser({...req.body});
-    res.status(201).send({ok:true})
+    const found = await getUser(user => user.id == req.params.id);
+    if(!found){
+        return res.status(404).send({message:"not user found"})
+    }
+    await editUser({...req.body}, req.params.id);
+    res.status(201).send({ok:true}) 
 })
 
 app.delete('/users/:id', async(req, res)=>{
     const {id} = req.params
-    const found = await searchUser(user => user.id == id);
+    const found = await getUser(user => user.id == id);
     if(!found){
         return res.status(404).send({message:"not user found"})
     }
